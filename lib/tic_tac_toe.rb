710eecaa2486666a31206fa3c01ad712a83c6fc3
Nemanja Drakulovic <ndrakulovic@hotmail.com>
2017-10-01 00:24:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,6 +71,9 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board,index) == true
     move(board,index,current_player(board))
+    if won?(board)
+      return "Congradulations #{winner(board)}!"
+    end
   else
     turn(board)
   end
