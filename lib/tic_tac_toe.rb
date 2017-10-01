c243c1810416cc0ee57dccd12b7be2a5c1da5a87
Nemanja Drakulovic <ndrakulovic@hotmail.com>
2017-10-01 00:18:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -96,23 +96,27 @@ def current_player(board)
   end
 end
 
+# def play(board)
+#   counter = 0
+#   until counter == 9
+#     if !(over?(board))
+#       turn(board)
+#       counter+= 1
+#     else
+#       break
+#     end
+#   end
+#   if won?(board)
+#     return "Congradulations #{winner(board)}!"
+#   end
+#   if draw?(board)
+#     puts "Cat's Game!"
+#   end
+# end
 def play(board)
-  counter = 0
-  until counter == 9
-    if !(over?(board))
-      turn(board)
-      counter+= 1
-      if draw?(board)
-        breaks
-      end
-    else
-      break
-    end
-  end
-  if won?(board)
-    return "Congradulations #{winner(board)}!"
-  end
-  if draw?(board)
-    puts "Cat's Game!"
+  turn_count = 0
+  while turn_count < 9
+    turn(board)
+    turn_count+=1
   end
 end
